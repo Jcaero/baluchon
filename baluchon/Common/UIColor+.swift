588a4a86d1f22b1d3c17be758c3daf87a7164b9b
pierrick <pierrickviret@gmail.com>
2023-07-29 21:31:45 +0200

    add extension Color

---
+++ baluchon/Common/UIColor+.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+.swift
+//  baluchon
+//
+//  Created by pierrick viret on 29/07/2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let navy = UIColor(red: 0.15, green: 0.29, blue: 0.36, alpha: 1.00)
+    static let pearlGrey = UIColor(red: 0.90, green: 0.90, blue: 0.86, alpha: 1.00)
+}
