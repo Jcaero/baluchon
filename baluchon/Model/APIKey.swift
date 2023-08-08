ba9abf2e6ffb4bdfbfbfaf2ab5d6136df92f8731
pierrick <pierrickviret@gmail.com>
2023-08-08 06:37:44 +0200

    test APIKey

---
+++ baluchon/Model/APIKey.swift
@@ -0,0 +1,17 @@
+//
+//  APIKey.swift
+//  baluchon
+//
+//  Created by pierrick viret on 08/08/2023.
+//
+
+import Foundation
+extension API {
+    enum APIKey {
+        case exchange
+        
+        var user: String {
+            return "test"
+        }
+    }
+}
